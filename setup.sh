9a686ee59249deb3db462d3c69ee9acf1ea44caf
Simental Magana, Marcos <mrkzmrkz@gmail.com>
2017-08-17 11:32:12 -0500

    create setup.sh script
    Signed-off-by: Simental Magana, Marcos <mrkzmrkz@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+managed=(shrc ssh tmux vim)
+
+command -v stow >/dev/null 2>&1 || { echo >&2 "Install stow to continue"; exit 1; }
+
+for element in ${managed[@]}; do
+    stow -t $HOME ${element}
+done
+unset element

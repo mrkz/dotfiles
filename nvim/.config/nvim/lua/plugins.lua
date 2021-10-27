1ef167ebd4187bbf80ac19ec41fab3a2ecd15d60
Simental Magana, Marcos <mrkzmrkz@gmail.com>
2021-10-26 23:43:13 -0500

    nvim: initial WIP for using packer
    Signed-off-by: Simental Magana, Marcos <mrkzmrkz@gmail.com>

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,77 @@
+--
+-- packer plugin manager bootstrap
+--
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local packer_install_dir = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+local repo = 'https://github.com/wbthomason/packer.nvim'
+local install_cmd = string.format('10split |term git clone --depth=1 %s %s', repo, packer_install_dir)
+
+if fn.empty(fn.glob(packer_install_dir)) > 0 then
+  execute(install_cmd)
+  execute 'packadd packer.nvim'
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+--
+-- plugins install
+-- 
+require("packer").startup({
+  function(use)
+    -- packer
+    use({"wbthomason/packer.nvim",
+         opt = true
+    })
+    -- colorscheme
+    use({"tanvirtin/monokai.nvim"})
+    -- on steroids
+    --   surround
+    use({"tpope/vim-surround",
+         opt = true
+    })
+    --   repeat
+    use({"tpope/vim-repeat",
+         opt = true
+    })
+    -- IDE look 'n feel
+    --   file tree viewer
+    use({"preservim/nerdtree",
+         opt = true,
+	 cmd = {"NERDTreeToggle", "NERDTree"}
+    })
+    --   view buffer functions/definitions
+    use({"majutsushi/tagbar",
+         opt = true,
+	 cmd = {"TagbarToggle", "Tagbar"}
+    })
+    -- markdown syntax
+    use({"plasticboy/vim-markdown",
+         opt = true,
+	 ft = {"markdown"}
+    })
+    -- git
+    --   +++/--- column of changes
+    use({"airblade/vim-gitgutter"})
+    --   cmdline
+    use({"tpope/vim-fugitive"})
+    -- statusline
+    use({"vim-airline/vim-airline"})
+    -- snippets
+    use({"SirVer/ultisnips",
+         opt = true,
+	 ft = {"cpp", "c", "python", "go", "rust", "lua"},
+	 requires = {{"honza/vim-snippets", opt = true}}
+    })
+    -- TreeSiter
+    use({"nvim-treesitter/nvim-treesitter",
+	 run = ":TSUpdate"
+    })
+    -- golang
+    use({"fatih/vim-go",
+    	 run = ":GoInstallBinaries"
+    })
+
+  end
+})
